3926ab0b993a3ab5c4e5df046de3b703b7ac4d25
Takayanagi5200 <113810035+Takayanagi5200@users.noreply.github.com>
2026-08-30 09:58:03 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 const http = require("http");
+
 const server = http.createServer((req,res) => {
-    res.end("Hello node.js!");
+
+
+    if (req.url === "/"){
+        res.end("Hello!");
+    }
+
+    else if (req.url === "/about") {
+        res.end("This is about page.");
+    }
+
+    else if (req.url === "/api/todos") {
+        res.end("Todo API");
+    }
+
+    else {
+        res.statusCode = 404;
+        res.end("Not Found");
+    }
 });
+
 server.listen(3000);
